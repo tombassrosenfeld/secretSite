4e6dd31686ac1772911630460f71b543c559a85f
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2019-02-13 17:48:00 +0000

    Added functions and tidied up

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
- <html>
- <head>
- 	<title>My Secret Site</title>
- </head>
- <body>
-
- 	<header>
- 		<h1>My Secret Site</h1>
- 	</header>
-
-
 <?php 
-	
-	$KNOWN_EMAIL = "myemail@myemailprovider.com";
 
-	$KNOWN_PASSWORD = "letmein";
-
-	if (!empty($_POST) && $_POST['email'] === $KNOWN_EMAIL && $_POST['password'] === $KNOWN_PASSWORD) {
-		?>
-		<h2>Welcome to the secrets!</h2>
-
-<?php
-	} else{
+	function printLoginForm() {
 ?>
 
 		<form method="post" >
@@ -40,17 +19,83 @@
 	 		</p>
  		</form>
 <?php
+	 	
+	}
+
+
+	function printLoginError() {
+
+?>
+
+			<h3>Something went wrong. Please try again</h3>
+			
+<?php  
+
+	}
+
+	function printSuccessHTML() {
+
+?>
+		<h2>Welcome to the secrets!</h2>
+<?php
+
+	}
+
+	function isLogInValid($email, $password) {
+
+		$KNOWN_EMAIL = "myemail@myemailprovider.com";
+
+		$KNOWN_PASSWORD = "letmein";
+
+		if ($email === $KNOWN_EMAIL && $password === $KNOWN_PASSWORD) {
+
+		return true;
+
+		} else {
+
+			false;
+		}
+
 	}
 ?>
-	
-
- 
 
 
 
- 
+<!-- BEGINNING OF HTML -->
 
- 	
- 
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>My Secret Site</title>
+ </head>
+ <body>
+
+ 	<header>
+ 		<h1>My Secret Site</h1>
+ 	</header>
+
+
+<?php
+	// if form was submitted...
+	if (!empty($_POST)) {
+		// if form was valid
+		if (isLogInValid($_POST['email'], $_POST['password'])) {
+			// show success HTML
+			printSuccessHTML();
+		// else
+		} else {
+			// show error message
+			printLoginError();
+			// show form HTML		
+			printLoginForm();
+		}
+	// else
+	} else {
+		// show form HTML
+		printLoginForm();
+	}
+
+?>
+
  </body>
  </html>
